5fbe0b3c398dead41ac9dad42d58eb9752e8dcb3
clairelubash <14023924+clairelubash@users.noreply.github.com>
2023-12-19 14:20:35 -0500

    add dirs and duration to ui

--- main.py
+++ main.py
@@ -1,76 +1,12 @@
-import googlemaps
-import os
 from dotenv import load_dotenv
 from utils import *
 from flask import Flask, request, render_template
-import folium
+
 
 load_dotenv()
 
 
 app = Flask(__name__)
-
-
-def get_optimized_route(starting_ending_address, keyword_lst, k = 3):
-
-    gmaps = googlemaps.Client(key = os.getenv('MAPS_API_KEY'))
-
-    start_lat, start_long = get_lat_long(gmaps, starting_ending_address)
-    start_coords = (start_lat, start_long)
-
-    lst_of_lsts = []
-
-    for key in keyword_lst:
-        places_lst = closest_from_keyword(gmaps, key, start_lat, start_long, k = k)
-        lst_of_lsts.append(places_lst)
-
-    df = create_waypoint_combo_df(lst_of_lsts)
-
-    df['duration'] = df['waypoints'].apply(lambda x: get_route_duration(gmaps, start_coords, x))
-
-    min_duration = df['duration'].min()
-    best_waypoints = df.loc[df['duration'] == min_duration, 'waypoints'].tolist()
-    best_waypoints_add = df.loc[df['duration'] == min_duration, 'addresses'].tolist()
-    best_waypoints_names = df.loc[df['duration'] == min_duration, 'names'].tolist()
-
-    optimized_route = [start_coords] + best_waypoints[0] + [start_coords]
-
-    optimized_route_dict = {'origin': {'coordinates': start_coords, 'address': starting_ending_address, 'name': 'Origin & Destination'}}
-
-    for idx, coord_pair in enumerate(optimized_route[1:-1]):
-        stop_num = f'Stop #{idx+1}'.format()
-
-        if stop_num not in optimized_route_dict:
-            optimized_route_dict[stop_num] = {}
-
-        optimized_route_dict[stop_num]['coordinates'] = coord_pair
-        optimized_route_dict[stop_num]['address'] = best_waypoints_add[0][idx][0]
-        optimized_route_dict[stop_num]['name'] = best_waypoints_names[0][idx][0]
-
-    optimized_route_dict['destination'] = {'coordinates': start_coords, 'address': starting_ending_address, 'name': 'Origin & Destination'}
-
-    return optimized_route_dict
-
-
-def generate_map(coordinates_dict):
-
-    coordinates = [sub_dict['coordinates'] for sub_dict in coordinates_dict.values() if 'coordinates' in sub_dict]
-
-    map = folium.Map(location = coordinates[0], zoom_start = 12)
-
-    for i in range(len(coordinates_dict)):
-        location = coordinates[i]
-        if 'address' in list(coordinates_dict.values())[i]:
-            add_value = list(coordinates_dict.values())[i]['address']
-        if 'name' in list(coordinates_dict.values())[i]:
-            name_value = list(coordinates_dict.values())[i]['name']
-        if i == 0 or i == (len(coordinates) - 1):
-            tag = name_value + '\n' + add_value
-        else:
-            tag = f'Stop #{i}:'.format() + '\n' + name_value + '\n' + add_value
-        folium.Marker(location, popup = tag).add_to(map)
-
-    return map
 
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -80,14 +16,20 @@ def index():
         param1 = str(request.form['param1'])
         param2 = [x.strip() for x in request.form['param2'].split(',')]
         
-        result = get_optimized_route(param1, param2)
+        result, dirs, total_duration = get_optimized_route(param1, param2)
+
+        formatted_dirs = format_instructions(dirs)
+        formatted_duration = str(total_duration) + ' minutes'
 
         generated_map = generate_map(result)
 
         map_file = "templates/map.html"
         generated_map.save(map_file)
 
-        return render_template('index.html')
+        return render_template('index.html', 
+                               result = formatted_dirs, 
+                               total_duration = formatted_duration
+                               )
 
     return render_template('input.html')
 
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import pandas as pd
 from itertools import product
+import folium
+import googlemaps
+import os
 
 
 def get_lat_long(gmaps, address):
@@ -112,7 +115,7 @@ def get_route_duration(gmaps, start_end_address, waypoints):
     for i in range(len(dirs[0]['legs'])):
         total_duration += convert_to_minutes(dirs[0]['legs'][i]['duration']['text'])
 
-    return total_duration
+    return dirs, total_duration
 
 
 
@@ -134,3 +137,117 @@ def create_waypoint_combo_df(list_of_lists):
     df = pd.DataFrame({'waypoints': pairs, 'addresses': addresses, 'names': names})
 
     return df
+
+
+
+def get_optimized_route(starting_ending_address, keyword_lst, k = 3):
+
+    """calls the places api to get the best waypoints for minimized driving time
+
+    Args:
+        starting_ending_address (str): starting point for user
+        keyword_lst (list): list of waypoints the user chose
+        k (int, optional): number of places to return. Defaults to 3.
+
+    Returns:
+        optimized_route_dict (dictionary): dictionary of coords and names for optimized places
+        dirs[0][0]["legs"] (dictionary): portion of dict describing the legs of journey with directions
+        min_duration (int): minimum driving time possible with waypoints
+    """
+
+    gmaps = googlemaps.Client(key = os.getenv('MAPS_API_KEY'))
+
+    start_lat, start_long = get_lat_long(gmaps, starting_ending_address)
+    start_coords = (start_lat, start_long)
+
+    lst_of_lsts = []
+
+    for key in keyword_lst:
+        places_lst = closest_from_keyword(gmaps, key, start_lat, start_long, k = k)
+        lst_of_lsts.append(places_lst)
+
+    df = create_waypoint_combo_df(lst_of_lsts)
+
+    df['duration'] = df['waypoints'].apply(lambda x: get_route_duration(gmaps, start_coords, x)[1])
+
+    min_duration = df['duration'].min()
+    best_waypoints = df.loc[df['duration'] == min_duration, 'waypoints'].tolist()
+    best_waypoints_add = df.loc[df['duration'] == min_duration, 'addresses'].tolist()
+    best_waypoints_names = df.loc[df['duration'] == min_duration, 'names'].tolist()
+
+    optimized_route = [start_coords] + best_waypoints[0] + [start_coords]
+
+    optimized_route_dict = {'origin': {'coordinates': start_coords, 'address': starting_ending_address, 'name': 'Origin & Destination'}}
+
+    for idx, coord_pair in enumerate(optimized_route[1:-1]):
+        stop_num = f'Stop #{idx+1}'.format()
+
+        if stop_num not in optimized_route_dict:
+            optimized_route_dict[stop_num] = {}
+
+        optimized_route_dict[stop_num]['coordinates'] = coord_pair
+        optimized_route_dict[stop_num]['address'] = best_waypoints_add[0][idx][0]
+        optimized_route_dict[stop_num]['name'] = best_waypoints_names[0][idx][0]
+
+    optimized_route_dict['destination'] = {'coordinates': start_coords, 'address': starting_ending_address, 'name': 'Origin & Destination'}
+
+    dirs = get_route_duration(gmaps, start_coords, optimized_route)
+
+    return optimized_route_dict, dirs[0][0]["legs"], min_duration
+
+
+def generate_map(coordinates_dict):
+
+    """creates folium map from route coordinates
+
+    Args:
+        coordinates_dict (dictionary): mapping of stops and their coordinates
+
+    Returns:
+        map: folium map for UI
+    """
+
+    coordinates = [sub_dict['coordinates'] for sub_dict in coordinates_dict.values() if 'coordinates' in sub_dict]
+
+    map = folium.Map(location = coordinates[0], zoom_start = 12)
+
+    for i in range(len(coordinates_dict)):
+        location = coordinates[i]
+        if 'address' in list(coordinates_dict.values())[i]:
+            add_value = list(coordinates_dict.values())[i]['address']
+        if 'name' in list(coordinates_dict.values())[i]:
+            name_value = list(coordinates_dict.values())[i]['name']
+        if i == 0 or i == (len(coordinates) - 1):
+            tag = '''
+            <b> {name_value}: </b> <br />
+            {add_value}
+            '''.format(name_value = name_value, add_value = add_value)
+            icon = folium.Icon(icon = 'home', prefix = 'fa', color = 'red')
+        else:
+            tag = '''
+            <b>Stop #{stop_num}: </b> {name_value} <br />
+            <b>Address: </b> {add_value}
+            '''.format(stop_num = i, name_value = name_value, add_value = add_value)
+            icon = None
+
+        folium.Marker(location, popup = folium.Popup(tag, parse_html = False,  max_width = 500), icon = icon).add_to(map)
+        folium.PolyLine(coordinates, weight = 5, opacity = 1).add_to(map)
+
+    return map
+
+
+
+def format_instructions(directions_dict):
+
+    """grabs all instructions and combines them into one string for UI
+
+    Args:
+        directions_dict (dictionary): dictionary of directions pulled from directions api
+
+    Returns:
+        html_instructions_combined (str): directions output for UI
+    """
+
+    html_instructions_combined = ''.join(step.get('html_instructions', '') + '\n' for element in directions_dict for step in element.get('steps', []))
+    
+    return html_instructions_combined
