d0ee75374349656a1c5fa5f23c31f8ed4e1873c6
clairelubash <14023924+clairelubash@users.noreply.github.com>
2023-12-19 22:18:51 -0500

    ui improvements

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from dotenv import load_dotenv
 from utils import *
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for
 
-
-load_dotenv()
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -19,6 +18,7 @@ def index():
         result, dirs, total_duration = get_optimized_route(param1, param2)
 
         formatted_dirs = format_instructions(dirs)
+        session['formatted_dirs'] = formatted_dirs
         formatted_duration = str(total_duration) + ' minutes'
 
         itinerary = '\n\n'.join(f"{key.capitalize() if key in ['origin', 'destination'] else value['name'].split(':')[0]}: {value['address'].title()}" for key, value in result.items())
@@ -35,6 +35,16 @@ def index():
                                )
 
     return render_template('input.html')
+
+
+@app.route('/show_directions', methods=['GET'])
+def show_directions():
+
+    formatted_dirs = session.get('formatted_dirs')
+    if formatted_dirs is None:
+        return redirect(url_for('index'))
+
+    return render_template('directions.html', formatted_dirs=formatted_dirs)
 
 
 
---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function goBack() {
+    window.history.back();
+}
